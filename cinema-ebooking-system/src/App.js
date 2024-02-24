4738855e00dbd370744463906660d8be4022c5fb
Daniel Gregory <danielgregory12345@gmail.com>
2024-02-24 13:08:14 -0500

    test

--- cinema-ebooking-system/src/App.js
+++ cinema-ebooking-system/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
 import SignUp from "./SignUp";
+import Verification from "./Verification";
 
 function App() {
   return (
