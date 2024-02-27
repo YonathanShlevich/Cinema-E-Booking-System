216045d285c2c0b74bc79390b90fb1b09b878ea6
CalvinTanudjaja <calvintan213@gmail.com>
2024-02-27 18:29:15 -0500

    initialdbconnecttest

--- cinema-ebooking-system/src/Home.js
+++ cinema-ebooking-system/src/Home.js
@@ -1,21 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import './Home.css';
+
+class Movie {
+  constructor(title, poster, trailerId, status) {
+    this.title = title;
+    this.poster = poster;
+    this.trailerId = trailerId;
+    this.status = status;
+  }
+}
 
 const Home = () => {
   const [isOpen, setOpen] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
 
-  const nowShowingMovies = [
-    { id: 1, title: 'Ready Player One', poster: '/rp1.jpg', trailerId: 'cSp1dM2Vj48' },
-    { id: 2, title: 'Rush Hour', poster: '/rush hour poster.jpg', trailerId: 'JMiFsFQcFLE' },
-  ];
+  useEffect(() => {
+    const fetchMovies = async () => {
 
-  const comingSoonMovies = [
-    { id: 3, title: 'Morbius', poster: '/morb.jpg', trailerId: 'oZ6iiRrz1SY' },
-    { id: 4, title: 'The Bee Movie', poster: '/beemovie.webp', trailerId: 'sxuU39hMpP4' },
-  ];
+      //placeholder to check if setmovies and fetchmovies work
+      const data = [
+        { title: 'Ready Player One', poster: '/rp1.jpg', trailerId: 'cSp1dM2Vj48', status: 'nowShowing' },
+        { title: 'Rush Hour', poster: '/rush hour poster.jpg', trailerId: 'JMiFsFQcFLE', status: 'nowShowing' },
+        { title: 'Morbius', poster: '/morb.jpg', trailerId: 'oZ6iiRrz1SY', status: 'comingSoon' },
+        { title: 'The Bee Movie', poster: '/beemovie.webp', trailerId: 'sxuU39hMpP4', status: 'comingSoon' },
+      ];
+
+      const moviesData = data.map((movie) => new Movie(movie.title, movie.poster, movie.trailerId, movie.status));
+      setMovies(moviesData);
+    };
+    fetchMovies();
+  }, []);
 
   const playTrailer = (trailerId) => {
     setSelectedMovie(trailerId);
@@ -31,11 +48,8 @@ const Home = () => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredNowShowingMovies = nowShowingMovies.filter(movie =>
-    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  const filteredComingSoonMovies = comingSoonMovies.filter(movie =>
+  // Filter movies based on search term
+  const filteredMovies = movies.filter(movie =>
     movie.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -55,62 +69,65 @@ const Home = () => {
 
       <h1>Now Showing</h1>
       <div className="movie-gallery">
-        {filteredNowShowingMovies.map((movie) => (
-          <div key={movie.id} className="movie-item">
-            {isOpen && selectedMovie === movie.trailerId ? (
-              <>
-                <iframe
-                  title={movie.title}
-                  width="560"
-                  height="315"
-                  src={`https://www.youtube.com/embed/${movie.trailerId}`}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                  onClick={(e) => e.stopPropagation()} // Prevent click propagation to close the trailer
-                />
-                <div className="modal">
-                  <button onClick={closeTrailer}>Close</button>
-                </div>
-              </>
-            ) : (
-              <>
-                <img src={movie.poster} alt={movie.title} onClick={() => playTrailer(movie.trailerId)} />
-                <h3>{movie.title}</h3>
-              </>
-            )}
-          </div>
+        {filteredMovies.map((movie, index) => (
+          movie.status === 'nowShowing' && (
+            <div key={index + 1} className="movie-item">
+              {isOpen && selectedMovie === movie.trailerId ? (
+                <>
+                  <iframe
+                    title={movie.title}
+                    width="560"
+                    height="315"
+                    src={`https://www.youtube.com/embed/${movie.trailerId}`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    onClick={(e) => e.stopPropagation()} // Prevent click propagation to close the trailer
+                  />
+                  <div className="modal">
+                    <button onClick={closeTrailer}>Close</button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <img src={movie.poster} alt={movie.title} onClick={() => playTrailer(movie.trailerId)} />
+                  <h3>{movie.title}</h3>
+                </>
+              )}
+            </div>
+          )
         ))}
       </div>
 
       <h1>Coming Soon</h1>
       <div className="movie-gallery">
-        {filteredComingSoonMovies.map((movie) => (
-          <div key={movie.id} className="movie-item">
-            {isOpen && selectedMovie === movie.trailerId ? (
-              <>
-                <iframe
-                  title={movie.title}
-                  width="560"
-                  height="315"
-                  src={`https://www.youtube.com/embed/${movie.trailerId}`}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                  onClick={(e) => e.stopPropagation()} // Prevent click propagation to close the trailer
-                />
-                <div className="modal">
-                  <button onClick={closeTrailer}>Close</button>
-                </div>
-              </>
-            ) : (
-              <>
-                <img src={movie.poster} alt={movie.title} onClick={() => playTrailer(movie.trailerId)} />
-                <h3>{movie.title}</h3>
-              </>
-            )}
-          </div>
+        {filteredMovies.map((movie, index) => (
+          movie.status === 'comingSoon' && (
+            <div key={index+1} className="movie-item">
+              {isOpen && selectedMovie === movie.trailerId ? (
+                <>
+                  <iframe
+                    title={movie.title}
+                    width="560"
+                    height="315"
+                    src={`https://www.youtube.com/embed/${movie.trailerId}`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    onClick={(e) => e.stopPropagation()} // Prevent click propagation to close the trailer
+                  />
+                  <div className="modal">
+                    <button onClick={closeTrailer}>Close</button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <img src={movie.poster} alt={movie.title} onClick={() => playTrailer(movie.trailerId)} />
+                  <h3>{movie.title}</h3>
+                </>
+              )}
+            </div>
+          )
         ))}
       </div>
-
     </div>
   );
 };
